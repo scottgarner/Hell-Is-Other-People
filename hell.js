4336590cf14d07bd5b6784a929255ef63ef9d7ab
Scott Garner <scott@j38.net>
2013-04-20 19:27:10 -0400

    DB insert and update

--- hell.js
+++ hell.js
@@ -5,16 +5,27 @@ var app = express();
 app.use(express.logger());
 app.use(express.bodyParser());
 
+client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
 app.post('/ws', function(request, response) {
   response.send('Total webservice!');
-  console.log(request.body.checkin.createdAt);
-  console.log(request.body.checkin.user.id);
-  console.log(request.body.checkin.venue.location.lat);
-  console.log(request.body.checkin.venue.location.lng);
+  var mod_time = console.log(request.body.checkin.createdAt);
+  var user_id = console.log(request.body.checkin.user.id);
+  var location_lat = console.log(request.body.checkin.venue.location.lat);
+  var location_lng = console.log(request.body.checkin.venue.location.lng);
+
+
+  client.query('UPDATE people SET mod_time = $2, location_lng = $3, location_lat = $4 WHERE user_id = $1', 
+	user_id, mod_time, location_lat, location_lng);
+  client.query('INSERT INTO people (user_id, mod_time, location_lat, location_lng) VALUES($1,$2,$3,$4) WHERE NOT EXISTS (SELECT 1 FROM people WHERE user_id = $1)',
+  	user_id, mod_time, location_lat, location_lng);
+
+
 });
 
 var port = process.env.PORT || 5000;

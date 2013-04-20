7b7d38c13bd4e4a6ab8b8278b09ad3a7ca579857
Scott Garner <scott@j38.net>
2013-04-20 18:42:24 -0400

    Postgress

--- hell.js
+++ hell.js
@@ -1,9 +1,15 @@
 var express = require("express");
+var pg = require('pg');
+
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
+});
+
+app.get('/ws', function(request, response) {
+  response.send('Total webservice!');
 });
 
 var port = process.env.PORT || 5000;

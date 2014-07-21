47a243746114e4c59fd0a30e91dc40b9e2790fbd
Scott Garner <scott@j38.net>
2014-07-21 11:20:45 -0500

    Update hell.js

--- hell.js
+++ hell.js
@@ -28,7 +28,8 @@ passport.deserializeUser(function(obj, done) {
 passport.use(new FoursquareStrategy({
     clientID: FOURSQUARE_CLIENT_ID,
     clientSecret: FOURSQUARE_CLIENT_SECRET,
-    callbackURL: callbackURL
+    callbackURL: callbackURL,
+    apiVersion: 20130424
   },
   function(accessToken, refreshToken, profile, done) {
     // asynchronous verification, for effect...
